2df204f4e2af25ebc553d263da2c320c44ad4b6c
Thomas Duthil <thomas.duthil@etu.u-bordeaux.fr>
2022-03-02 17:02:23 +0100

    Exercice QR Code Fini

---
+++ conteneurWeb/helloServlet/src/main/java/fr/iut/ListRoomServlet.java
@@ -0,0 +1,58 @@
+package fr.iut;
+
+import fr.iut.model.Room;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "listRoom", urlPatterns = {"/listRoom"})
+public class ListRoomServlet extends HttpServlet {
+    ArrayList<Room> fakeRooms = new ArrayList<>();
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+        Template freemarkerTemplate = null;
+        freemarker.template.Configuration freemarkerConfiguration =
+                new freemarker.template.Configuration();
+        freemarkerConfiguration.setClassForTemplateLoading(this.getClass(), "/");
+        freemarkerConfiguration.setObjectWrapper(new DefaultObjectWrapper());
+        try {
+            freemarkerTemplate =
+                    freemarkerConfiguration.getTemplate("templates/listRoom.ftl");
+        } catch (IOException e) {
+            System.out.println("Unable to process request,  error during freemarker template retrieval.");  }
+
+        Map<String, Object> root = new HashMap<String, Object>();
+        // navigation data and links
+        Room room = new Room("room1",0,15);
+        Room room2 = new Room("room2",2,16);
+        Room room3 = new Room("room100", 25,100);
+        fakeRooms.add(room);
+        fakeRooms.add(room2);
+        fakeRooms.add(room3);
+        root.put("title", "LIST FAKE ROOM");
+        root.put("fakeRooms", fakeRooms);
+        PrintWriter out = response.getWriter();
+        assert freemarkerTemplate != null;
+        try {
+            freemarkerTemplate.process(root, out);
+            out.close();}
+        catch (TemplateException e) { e.printStackTrace(); }
+        // set mime type
+        response.setContentType("text/html");
+    }
+}
---
+++ conteneurWeb/helloServlet/src/main/java/fr/iut/model/Room.java
@@ -0,0 +1,37 @@
+package fr.iut.model;
+
+public class Room {
+    String name;
+    int occupation;
+    int capacity;
+
+    public Room(String name, int occupation, int capacity) {
+        this.name = name;
+        this.occupation = occupation;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(int occupation) {
+        this.occupation = occupation;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
